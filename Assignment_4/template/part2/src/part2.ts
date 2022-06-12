7f6fa9e0e601b19501ebfc1d7f27044917823b34
tomcooll <tomlipshits5@gmail.com>
2022-06-12 19:20:27 +0300

    passing tests in 2.1, 2.2, 2.4

--- Assignment_4/template/part2/src/part2.ts
+++ Assignment_4/template/part2/src/part2.ts
@@ -15,40 +15,78 @@ export function makeTableService<T>(sync: (table?: Table<T>) => Promise<Table<T>
     return {
         get(key: string): Promise<T> {
             let p = new Promise<T>((resolve, reject) => {
-                sync().then((table)=>{
+                sync().then((table:Table<T>)=>{
                     if (key in table){
                         resolve(table[key])
                     }else{
-                        reject()
+                        reject(MISSING_KEY)
                     }
                 }).catch(()=>{return MISSING_KEY})
             })
-            p.then((value)=>{
+            p.then((value: T)=>{
                 return value
-            }).catch(()=>{return MISSING_KEY})
+            }).catch((massage: string)=>{return massage})
             return p
         },
         set(key: string, val: T): Promise<void> {
             let p = new Promise<void>((resolve, reject) => {
                 sync().then((table: Table<T>) => {
-                    let record ={
-                        key: val
-                    }
+                    let record : Record<string, T> =  {}
                     Object.assign(record, table)
-                    sync(record)
+                    record[key] = val
+                    sync(record).then(()=>{}).catch(()=>{})
+                    resolve()
                 }).catch(()=>{return MISSING_KEY})
             })
+            p.then(()=>{return}).catch(()=>{return})
             return p
         },
         delete(key: string): Promise<void> {
-            return Promise.reject('not implemented')
+            let p = new Promise<void>((resolve, reject) => {
+                sync().then((table: Table<T>) => {
+                    if (key in table) {
+                        const filtered = Object.keys(table)
+                            .filter((currKey: string): Boolean => currKey != key)
+                            .reduce((obj: Record<string, T>, currKey: string) => {
+                                obj[currKey] = table[currKey]
+                                return obj;
+                            }, {})
+                        sync(filtered)
+                        resolve()
+                    } else {
+                        reject(MISSING_KEY)
+                    }
+
+                }).catch(() => {
+                    return MISSING_KEY
+                })
+            })
+            p.then(() => {
+                return
+            }).catch((message) => {
+                return message
+            })
+            return p
         }
     }
 }
 
 // Q 2.1 (b)
 export function getAll<T>(store: TableService<T>, keys: string[]): Promise<T[]> {
-    return Promise.reject('not implemented')
+    let p = new Promise<T[]>((resolve, reject) => {
+        const arr : T[] = []
+        for (let i = 0 ; i< keys.length ; i++){
+            store.get(keys[i]).then(function (result:T){
+                arr.push(result)
+                if (i == keys.length -1){
+                    resolve(arr)
+                }
+            }).catch(()=>{reject(MISSING_KEY)});
+        }
+    });
+    p.then((arr: T[]) => {return arr}).catch((message:string)=>{return message})
+    return p
+
 }
 
 
@@ -63,10 +101,28 @@ export function isReference<T>(obj: T | Reference): obj is Reference {
 
 export async function constructObjectFromTables(tables: TableServiceTable, ref: Reference) {
     async function deref(ref: Reference) {
-        return Promise.reject('not implemented')
+        let tableService = tables[ref.table]
+        if(tableService == undefined){
+            return Promise.reject(MISSING_TABLE_SERVICE)
+        }
+        try {
+            let obj: Record<string, any> = await tableService.get(ref.key)
+            for (const [key, val] of Object.entries(obj)){
+                if (isReference(val)){
+                    try{
+                        obj[key] = await deref(val)
+                    }catch(err){
+                        return Promise.reject(err)
+                    }
+                }
+            }
+            return obj
+        }catch (err){
+            return Promise.reject(err)
+        }
     }
 
-    return deref(ref)
+    return await deref(ref)
 }
 
 // Q 2.3
@@ -93,11 +149,24 @@ export type ReactiveTableService<T> = {
 
 export async function makeReactiveTableService<T>(sync: (table?: Table<T>) => Promise<Table<T>>, optimistic: boolean): Promise<ReactiveTableService<T>> {
     // optional initialization code
+    let _observer: (table: Table<T>) => void
+    let _table = await sync()
 
-    let _table: Table<T> = await sync()
+
+
 
     const handleMutation = async (newTable: Table<T>) => {
-        // TODO implement!
+        try{
+            _table = await sync(newTable)
+            if (!optimistic && _observer != null){
+                _observer(newTable)
+            }
+        }catch (err) {
+            if(_observer != null){
+                _observer(_table)
+            }
+            return Promise.reject(err)
+        }
     }
     return {
         get(key: string): T {
@@ -108,14 +177,38 @@ export async function makeReactiveTableService<T>(sync: (table?: Table<T>) => Pr
             }
         },
         set(key: string, val: T): Promise<void> {
-            return handleMutation(null as any /* TODO */)
+            const record: Record<string, T> = {}
+            try{
+                Object.assign(record, _table)
+                record[key] = val
+                if (optimistic && _observer != null){
+                    _observer(record)
+                }
+                return handleMutation(record)
+            }catch(err){
+                return handleMutation(record)
+            }
         },
+
         delete(key: string): Promise<void> {
-            return handleMutation(null as any /* TODO */)
+            const filtered: Record<string, T> = {}
+            try{
+                for (const [currKey, val] of Object.entries(_table)){
+                    if (currKey != key){
+                        filtered[currKey] = val
+                    }
+                }
+                if (optimistic && _observer != null){
+                    _observer(filtered)
+                }
+                return handleMutation(filtered)
+            }catch(err){
+                return handleMutation(filtered)
+            }
         },
 
         subscribe(observer: (table: Table<T>) => void): void {
-            // TODO implement!
+            _observer = observer
         }
     }
 }
